d6a78a1e1069dba75884c0d85199c5d7296cc554
My Solutions 4 U <mysolutions4u@mys-mac-mini.local>
2017-01-13 10:44:26 +0530

    Initial Commit

---
+++ PropertyRental/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PropertyRental
+//
+//  Created by My Solutions 4 U on 1/13/17.
+//  Copyright © 2017 My Solutions 4 U. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
