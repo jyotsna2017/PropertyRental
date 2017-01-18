4f5d4b48eb685b597674eafb4e74875a2859475b
My Solutions 4 U <mysolutions4u@Mys-Mac-mini.local>
2017-01-18 11:29:49 +0530

    first commit

---
+++ PropertyRental/Vendor.swift
@@ -0,0 +1,13 @@
+//
+//  Vendor.swift
+//  PropertyRental
+//
+//  Created by My Solutions 4 U on 1/16/17.
+//  Copyright © 2017 My Solutions 4 U. All rights reserved.
+//
+
+import UIKit
+
+class Vendor: NSObject {
+
+}
